de66b7a7fc41d04f495c38319d0c6074a499f1e3
Ravjot <ravjot28@gmail.com>
2016-11-06 17:06:29 -0500

    Removed \r in the text to load in html

--- src/main/java/com/rav/audtioapp/dao/param/ParamsDAO.java
+++ src/main/java/com/rav/audtioapp/dao/param/ParamsDAO.java
@@ -144,7 +144,7 @@ public class ParamsDAO {
 			statement.setString(index++, paramName);
 			ResultSet rs = statement.executeQuery();
 			while (rs.next()) {
-				paramValue = rs.getString(1);
+				paramValue = rs.getString(1).replaceAll("\r", " ");
 			}
 
 		} catch (Exception e) {
@@ -178,7 +178,7 @@ public class ParamsDAO {
 			statement = conn.prepareStatement(sql);
 			ResultSet rs = statement.executeQuery();
 			while (rs.next()) {
-				paramValue += rs.getString(1) + "---" + rs.getString(2) + "~~";
+				paramValue += rs.getString(1) + "---" + rs.getString(2).replaceAll("(\\r|\\n)", "") + "~~";
 			}
 
 		} catch (Exception e) {
